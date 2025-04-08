33acd1820d79ce0a298dad6ec1cf357c39e4170a
Bianka <kedvesemail@gmail.com>
2025-04-08 08:53:14 +0200

    2025.04.07-ről

---
+++ src/main/java/com/example/DialogController.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DialogController {
+
+    @FXML
+    void onClickCloseButton(ActionEvent event) {
+        
+
+    }
+
+}
