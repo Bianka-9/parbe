a0ef7f103d75398f6d44071acccd3800e277c875
Bianka <kedvesemail@gmail.com>
2025-04-08 10:12:30 +0200

    MainController látszik a Dialog-ban

--- src/main/java/com/example/DialogController.java
+++ src/main/java/com/example/DialogController.java
@@ -2,13 +2,15 @@ package com.example;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.stage.Stage;
 
 public class DialogController {
 
     @FXML
     void onClickCloseButton(ActionEvent event) {
-        
-
+        Stage stage = (Stage) (((javafx.scene.Node) event.getSource()).getScene().getWindow());
+        stage.close();
+        //((Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow()).close();
     }
 
 }
