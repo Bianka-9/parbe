bdac3be87d79cbeaf1c6c566a3dbfd0e9a7bcb13
Bianka <kedvesemail@gmail.com>
2025-04-08 10:40:42 +0200

    Mentés gomb hozzáadva

--- src/main/java/com/example/DialogController.java
+++ src/main/java/com/example/DialogController.java
@@ -2,15 +2,30 @@ package com.example;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class DialogController {
 
+    MainController mainController;
+
+    
     @FXML
-    void onClickCloseButton(ActionEvent event) {
-        Stage stage = (Stage) (((javafx.scene.Node) event.getSource()).getScene().getWindow());
-        stage.close();
-        //((Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow()).close();
+    public TextField plateField;
+
+    @FXML
+    void onClickSaveButton(ActionEvent event) {
+        // Stage stage = (Stage) (((javafx.scene.Node) event.getSource()).getScene().getWindow());
+        // stage.close();
+
+
+        mainController.carList.getItems().add(plateField.getText());
+        mainController.stage.close();
+
+    }
+
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
     }
 
 }
--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -6,12 +6,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 public class MainController {
 
+    Stage stage = new Stage();
+
     @FXML
-    void onClickShowButton(ActionEvent event) { 
+    public ListView<String> carList;
+
+    @FXML
+    void onClickAddButton(ActionEvent event) { 
         showDialog();
     }
 
@@ -26,9 +32,12 @@ public class MainController {
     void tryShowDialog() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("dialogScene.fxml"));
         Scene scene = new Scene(loader.load());
-        Stage stage = new Stage();
+       
         stage.setScene(scene);
         stage.show();
+
+        DialogController dialogController = loader.getController();
+        dialogController.setMainController(this);
     }
 
 }
